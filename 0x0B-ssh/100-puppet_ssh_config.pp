667d7b2766ae4afe9b5ad67146c89eae01cf98d4
abelasfaw <abeloman89@gmail.com>
2022-12-11 12:09:24 -0800

    fixex task 4 pulblic key

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # puppet ssh configuration
 exec { 'ssh_config':
   path    => '/bin',
-  command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config; echo "IdentityFile ~/.ssh/holberton" >> /etc/ssh/ssh_config',
+  command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config; echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
 }
