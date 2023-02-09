8a80f51854b182a4ed56323dac5b695059513e7c
abelasfaw <abeloman89@gmail.com>
2023-02-09 19:31:51 +0000

    added task 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# Enable login using holberton user and open files
+
+exec { 'increase-hard-limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'increase-soft-limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
